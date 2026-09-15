89ffb2bfff2ac11a255fbe4b15fad9bc063d7e50
jmil449988-netizen <jmil449988@gmail.com>
2026-09-15 13:16:35 -0700

    Temp debug logging for level-up handler (app.js v73)

--- public/js/app.js
+++ public/js/app.js
@@ -3310,6 +3310,7 @@ channel.on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'user
 var prevLevel = userStats[p.new.user_id] ? userStats[p.new.user_id].level : null;
 userStats[p.new.user_id] = p.new;
 refreshLevelBadges(p.new.user_id);
+console.log('[gc-levelup-debug]', { userId: p.new.user_id, prevLevel: prevLevel, newLevel: p.new.level, hasPeople: !!people[p.new.user_id] });
 if (prevLevel != null && p.new.level > prevLevel && people[p.new.user_id]) {
 addSys('🎉 ' + people[p.new.user_id].name + ' reached Level ' + p.new.level + '!');
 }
